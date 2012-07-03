57fbe98155af0e8f52a41475d1bc59c00f02e862
Byron Sommardahl <byron@acklenavenue.com>
2012-07-03 13:00:24 -0500

    Added new nuspec.

--- src/AcklenAvenue.Data.NHibernate/Properties/AssemblyInfo.cs
+++ src/AcklenAvenue.Data.NHibernate/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AcklenAvenue.Data.NHibernate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An easy-to-use but opinionated data layer bootstrapper.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Acklen Avenue")]
-[assembly: AssemblyProduct("AcklenAvenue.Data.NHibernate.StructureMap")]
+[assembly: AssemblyProduct("AcklenAvenue.Data.NHibernate")]
 [assembly: AssemblyCopyright("Copyright © Acklen Avenue, LLC 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
