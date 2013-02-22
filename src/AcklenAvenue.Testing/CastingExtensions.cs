383b2d6441849d97cb9cea38a765ad11f647af2e
Byron Sommardahl <byron@acklenavenue.com>
2013-02-22 09:47:02 -0600

    Added As<> extension.

---
+++ src/AcklenAvenue.Testing/CastingExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AcklenAvenue.Testing
+{
+    public static class CastingExtensions
+    {
+        public static T As<T>(this object obj)
+        {
+            return (T) obj;
+        }
+    }
+}
---
+++ src/AcklenAvenue.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AcklenAvenue.Testing")]
+[assembly: AssemblyDescription("Extensions for Testing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acklen Avenue")]
+[assembly: AssemblyProduct("AcklenAvenue.Testing")]
+[assembly: AssemblyCopyright("Copyright © Acklen Avenue, LLC 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("79f17918-85c9-48b2-bc13-adc1a7742a63")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
