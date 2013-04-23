cdc9851660733aa8365880d4087e9cf90522bade
Byron Sommardahl <byron@acklenavenue.com>
2013-04-23 10:49:13 -0500

    Improving moq helper.

--- src/AcklenAvenue.Testing.Moq/ThatHas.cs
+++ src/AcklenAvenue.Testing.Moq/ThatHas.cs
@@ -6,11 +6,21 @@ namespace AcklenAvenue.Testing.Moq
 {
     public static class ThatHas
     {
-        public static Expression<Func<T, bool>> SimpleExpressionLike<T>(Expression<Func<T, bool>> expr)
+        public static Expression<Func<T, bool>> SimpleExpressionLike<T>(Expression<Func<T, bool>> expectedExpression)
         {
             return
                 Match.Create<Expression<Func<T, bool>>>(
-                    expression => expression.ToString() == expr.ToString());
+                    actualExpression =>
+                        {
+                            if (actualExpression.ToString() != expectedExpression.ToString())
+                            {
+                                throw new Exception(
+                                    string.Format(
+                                        "The expression that was passed in from the production code did not match the expression given in the spec/test. \r\nExpected: {0}\r\nActual: {1}\r\n\r\nNOTE: Remember that ThatHas.SimpleExpression compares the .ToString() values of the expressions. This means that the expression must be very simple and must match exactly on both sides (production and spec).",
+                                        actualExpression.ToString(), expectedExpression.ToString()));
+                            }
+                            return true;
+                        });
         }
 
         public static Expression<Func<T, bool>> AnyPredicateOf<T>()
